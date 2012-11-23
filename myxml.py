878a8f14e5541b01dac4bca8d4372587e0bc5ed2
raptorfin <dsgreene713@me.com>
2012-11-23 06:50:34 -0500

    revised get_values to take a generic iterable

--- myxml.py
+++ myxml.py
@@ -11,7 +11,7 @@ def query_for_element(dom, elem):
     return dom.getElementsByTagName(elem)
 
 
-def get_values(line, *args):
+def get_values(line, args):
     return {arg: line.getAttribute(arg) for arg in args}
 
 
@@ -19,7 +19,4 @@ if __name__ == '__main__':
     a = parse_xml('/Users/Dan/Downloads/DailyTradeConfirm_Test.xml')
     b = query_for_element(a, 'TradeConfirm')
     for line in b:
-        print(get_values(line,
-                         'description','symbol', 'orderID', 'assetCategory',
-                         'putCall', 'quantity', 'price', 'commission',
-                         'dateTime', 'code', 'buySell'))
+        print(get_values(line, ('description','symbol')))
